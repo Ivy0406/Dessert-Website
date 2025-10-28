853964eeb43f402b21cebab8913dd34b060cdfe8
Ivy Yang <ivy990030@gmail.com>
2025-10-28 16:17:11 +0800

    feat:add Product List render function

---
+++ js/main.js
@@ -0,0 +1,111 @@
+
+// 引入資料並宣告常用變數
+import data from "../products-data.js";
+let AllProductsData = [];
+AllProductsData = data;
+
+
+const PageSelect = document.getElementById("page-select");
+const SeriesSelect = document.getElementById("series-select");
+const ProductList = document.getElementById("product-list");
+let ItemsPerPage = 6;
+let CurrentPage = 1;
+
+
+
+// function 區
+
+// 功能：根據頁數決定渲染資料
+function showPage(CurrentPage){
+    const StartIndex = (CurrentPage-1)*ItemsPerPage;
+    const EndIndex = StartIndex+ItemsPerPage;
+    const PageData = AllProductsData.slice(StartIndex,EndIndex);
+    renderProductsList(PageData);
+}
+
+
+// 功能：渲染產品列表
+function renderProductsList(DataToShow){
+    let CardsHTML = ""; //每次渲染都要先清空原顯示的商品項目，否則會一直疊加
+    DataToShow.forEach(function(item){
+        CardsHTML = CardsHTML + ProductCard(item); //持續累加到最後一筆
+        // console.log(item);
+    })
+   ProductList.innerHTML = CardsHTML;
+}
+
+
+// 製作商品卡
+
+function ProductCard(CardToShow){
+    let Card = `<li>
+              <figure class="relative">
+                <div class="label absolute z-10 left-5">
+                  <span>${CardToShow.series}</span>
+                </div>
+                <img
+                  class="absolute z-10 right-5 top-[19px] hover:scale-150 cursor-pointer"
+                  src="./images/favorite_border.png"
+                  alt="加入我的收藏"
+                />
+                <img
+                  class="h-[315px] w-full object-cover"
+                  src="${CardToShow.picture}" alt="${CardToShow.name}"
+                />
+              </figure>
+              <figcaption class="flex">
+                <p class="product-name">${CardToShow.name}</p>
+                <p class="product-price">NT$${CardToShow.price}</p>
+              </figcaption>
+              <button class="lg-btn w-full">加入購物車</button>
+            </li>`;
+        // console.log(Card);
+
+    return Card;
+
+}
+
+
+
+
+
+// 事件監聽區
+
+PageSelect.addEventListener("click",function(event){
+    const ClickValue = event.target.value;
+    let TargetPage = CurrentPage;
+    if(!ClickValue){
+        return;
+    };
+    
+    if(ClickValue === "prev"){
+        TargetPage = TargetPage-1;
+    }
+    else if(ClickValue === "next"){
+        TargetPage = TargetPage+1;
+    }
+    else{
+        TargetPage = parseInt(ClickValue);
+    }
+
+
+    const TotalPages = Math.ceil(AllProductsData.length/ItemsPerPage);
+
+    if(TargetPage<1){
+        TargetPage = 1;
+    }
+    if(TargetPage > TotalPages){
+        TargetPage = TotalPages;
+    }
+    if(TargetPage !== CurrentPage){
+        CurrentPage = TargetPage;
+        showPage(CurrentPage);
+    }
+
+});
+
+
+// 執行區
+
+showPage(CurrentPage);
+
---
+++ products-data.js
@@ -0,0 +1,110 @@
+export default [
+    {
+        "name": "水果燕麥粥",
+        "series": "新品上市",
+        "price": 450,
+        "picture": "./images/products/p-(1).png"
+    },
+    {
+        "name": "糖霜甜甜圈",
+        "series": "本日精選",
+        "price": 450,
+        "picture": "./images/products/p-(2).png"
+    },
+    {
+        "name": "綜合莓果派",
+        "series": "人氣推薦",
+        "price": 450,
+        "picture": "./images/products/p-(6).png"
+    },
+    {
+        "name": "翻轉蘋果派",
+        "series": "人氣推薦",
+        "price": 450,
+        "picture": "./images/products/p-(3).png"
+    },
+    {
+        "name": "鮮奶油蛋糕",
+        "series": "本日精選",
+        "price": 450,
+        "picture": "./images/products/p-(4).png"
+    },
+    {
+        "name": "草莓奶酪杯",
+        "series": "新品上市",
+        "price": 450,
+        "picture": "./images/products/p-(5).png"
+    },
+    {
+        "name": "藍莓優格杯",
+        "series": "本日精選",
+        "price": 450,
+        "picture": "./images/products/p-(5).jpg"
+    },
+    {
+        "name": "巧克力布朗尼",
+        "series": "人氣推薦",
+        "price": 450,
+        "picture": "./images/products/p-(1).jpg"
+    },
+    {
+        "name": "抹茶紅豆塔",
+        "series": "新品上市",
+        "price": 450,
+        "picture": "./images/products/p-(6).png"
+    },
+    {
+        "name": "檸檬馬卡龍",
+        "series": "本日精選",
+        "price": 450,
+        "picture": "./images/products/p-(4).jpg"
+    },
+    {
+        "name": "經典起司蛋糕",
+        "series": "人氣推薦",
+        "price": 450,
+        "picture": "./images/products/p-(4).png"
+    },
+    {
+        "name": "法式可麗餅",
+        "series": "本日精選",
+        "price": 450,
+        "picture": "./images/products/p-(3).png"
+    },
+    {
+        "name": "焦糖布丁",
+        "series": "新品上市",
+        "price": 450,
+        "picture": "./images/products/p-(1).jpg"
+    },
+    {
+        "name": "提拉米蘇",
+        "series": "人氣推薦",
+        "price": 450,
+        "picture": "./images/products/p-(3).png"
+    },
+    {
+        "name": "熔岩巧克力蛋糕",
+        "series": "本日精選",
+        "price": 450,
+        "picture": "./images/products/p-(1).jpg"
+    },
+    {
+        "name": "芒果慕斯",
+        "series": "新品上市",
+        "price": 450,
+        "picture": "./images/products/p-(5).jpg"
+    },
+    {
+        "name": "杏仁可頌",
+        "series": "人氣推薦",
+        "price": 450,
+        "picture": "./images/products/p-(3).png"
+    },
+    {
+        "name": "繽紛水果塔",
+        "series": "本日精選",
+        "price": 450,
+        "picture": "./images/products/p-(2).jpg"
+    }
+]
