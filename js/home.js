c408cae7cf08e0df56d6b7aa6c70fbe1fb5d4ee8
Ivy Yang <ivy990030@gmail.com>
2025-10-30 22:43:42 +0800

    feat:add random product list on home page for mobile view

--- js/home.js
+++ js/home.js
@@ -7,8 +7,8 @@ let FiltedProduct = AllProductsData;
 
 
 const SeriesSelect = document.getElementById("series-select");
-const ProductList = document.querySelector(".product-list-web"); 
-console.log(ProductList);
+const ProductListWeb = document.querySelector(".product-list-web"); 
+const ProductListMobile = document.querySelector(".product-list-mobile")
 let ItemsPerPage = 3;
 
 // 初始狀態
@@ -60,19 +60,50 @@ function RandomSelection(FiltedData) {
 
 // 功能：渲染在商品列表區塊
 function renderProductsList(DataToShow) {
-  let CardsHTML = ""; //每次渲染都要先清空原顯示的商品項目，否則會一直疊加
+  let CardsHTMLweb = ""; //每次渲染都要先清空原顯示的商品項目，否則會一直疊加
+  let CardsHTMLmobile = ""
   DataToShow.forEach(function (item) {
-    CardsHTML = CardsHTML + ProductCard(item); //持續累加到最後一筆
+    CardsHTMLweb = CardsHTMLweb + ProductCardWeb(item); //持續累加到最後一筆
+    CardsHTMLmobile = CardsHTMLmobile + ProductCardMobile(item);
   });
 
-  ProductList.innerHTML = CardsHTML;
+  ProductListWeb.innerHTML = CardsHTMLweb;
+  ProductListMobile.innerHTML = CardsHTMLmobile;
 }
 
-// 功能：製作商品卡片
-function ProductCard(CardToShow) {
+// 功能：製作商品卡片(網頁版)
+function ProductCardWeb(CardToShow) {
   let Card = `<li class=" flex-1 max-w-[300px]">
       <figure class="relative">
           <div class="label absolute z-10 left-5">
+                <span>${CardToShow.series}</span>
+              </div>
+              <img
+                class="absolute z-10 right-5 top-[19px] hover:scale-150 cursor-pointer w-5"
+                src="./images/baseline-favorite-border-empty.svg"
+                alt="加入我的收藏"
+              />
+              <img
+                class="h-[315px] w-full object-cover"
+                src="${CardToShow.picture}" alt="${CardToShow.name}"
+              />
+            </figure>
+            <figcaption class="flex">
+              <p class="product-name">${CardToShow.name}</p>
+              <p class="product-price">NT$${CardToShow.price}</p>
+            </figcaption>
+            <button class="lg-btn w-full">加入購物車</button>
+          </li>`;
+  // console.log(Card);
+
+  return Card;
+}
+
+// 功能：製作商品卡片(行動版)
+function ProductCardMobile(CardToShow) {
+  let Card = `<li class="w-[315px] shrink-0 snap-start">
+            <figure class="relative">
+              <div class="label absolute z-10 left-5">
                 <span>${CardToShow.series}</span>
               </div>
               <img
